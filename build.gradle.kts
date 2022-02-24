9afbdb25d68ce00dfc8722b308a8cdb0857106f9
llidy <santoslid@gmailcom>
2022-02-24 00:55:10 -0300

    testes integrados

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     runtimeOnly("mysql:mysql-connector-java")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test:5.6.2")
 	testImplementation("io.mockk:mockk:1.12.0")
 }
 
--- src/test/kotlin/com/mercadolivro/customer/CustomerRepositoryTest.kt
+++ src/test/kotlin/com/mercadolivro/customer/CustomerRepositoryTest.kt
@@ -1,4 +1,81 @@
 package com.mercadolivro.customer
 
+import com.mercadolivro.helper.buildCustomer
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import kotlin.math.E
+
+@SpringBootTest
+@ActiveProfiles("test")
+@ExtendWith(MockKExtension::class)
 class CustomerRepositoryTest {
+
+    @Autowired
+    private lateinit var customerRepository: CustomerRepository
+
+    @BeforeEach
+    fun setup() = customerRepository.deleteAll()
+
+    @Test
+    fun `should return name containing`(){
+        val marcos = customerRepository.save(buildCustomer(name = "Marcos"))
+        val matheus = customerRepository.save(buildCustomer(name = "Matheus"))
+        customerRepository.save(buildCustomer(name = "Alex"))
+
+        val customer = customerRepository.findByNameContaining("Ma")
+
+        assertEquals(listOf(marcos, matheus), customer)
+    }
+
+    @Nested
+    inner class `exists by email` {
+        @Test
+        fun `should return true when email exists`() {
+            val email = "email@teste.com"
+            customerRepository.save(buildCustomer(email = email))
+
+            val exists = customerRepository.existsByEmail(email)
+
+            assertTrue(exists)
+        }
+
+        @Test
+        fun `should return false when email exists`() {
+            val email = "noneexistinfemail@teste.com"
+
+            val exists = customerRepository.existsByEmail(email)
+
+            assertFalse(exists)
+        }
+    }
+
+    @Nested
+    inner class `find by email` {
+        @Test
+        fun `should return customer when email exists`() {
+            val email = "email@teste.com"
+            val customer = customerRepository.save(buildCustomer(email = email))
+
+            val result = customerRepository.findByEmail(email)
+
+            assertNotNull(result)
+            assertEquals(customer, result)
+        }
+
+        @Test
+        fun `should return null when email exists`() {
+            val email = "noneexistinfemail@teste.com"
+
+            val result = customerRepository.findByEmail(email)
+
+            assertNull(result)
+        }
+    }
 }
